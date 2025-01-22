184c5c4d029cefbabe506a9fdf20723c4e4f6a97
joao vitor <joaomakenze@gmail.com>
2025-01-22 19:45:04 -0300

    criando funcao carregar dadosProdutos

--- app/main.js
+++ app/main.js
@@ -57,9 +57,9 @@ function renderizarProdutos () {
                                     <button class="btn rounded-full btn-danger">
                                         -
                                     </button>
-                                    <button class="btn rounded-full btn-secondary">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                                    <button data_id="${produto.id}" class="btn rounded-full btn-secondary" onclick='carregarDadosProduto(event)'>
+                                        <svg data_id="${produto.id}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                            <path data_id="${produto.id}" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                                             </svg>
                                     </button>
                                 </div>
@@ -74,3 +74,14 @@ function renderizarProdutos () {
 }
 
 renderizarProdutos();
+
+function carregarDadosProduto(event) {
+    const id = event.target.getAttribute('data_id');
+    const alterarProduto = localStorage.getItem('produtos');
+    const alterarDados = JSON.parse(alterarProduto);
+    const idProduto = alterarDados.find(Dados => Dados.id == id);
+
+
+    console.log(idProduto)
+
+} 
